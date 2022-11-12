64b49e18281e1247668769677441ed6fb091b60e
Bogdan Costea <bogdan.costea@x2mobile.net>
2022-11-12 12:27:37 +0200

    Initial Commit

---
+++ MemorizeApp/MemorizeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeAppApp.swift
+//  MemorizeApp
+//
+//  Created by Bogdan Costea on 12.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
